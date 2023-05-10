964bc290acdcbb2ab05428277c4d14dc0071a2f5
ZoeChrvx <zchervaux@artfx.fr>
2023-05-10 14:21:52 +0200

    PlayerMovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public static PlayerMovement instance;
+
     [Header("Information Player")]
     [Space]
-    public int health, speed, jump, damage;
+    public Rigidbody rb;
+    public int health, speed, damage;
+    public float jump;
+    Vector2 direction;
+
+    public GameObject sword;
+
+    bool isGrounded;
 
     public void Awake()
     {
@@ -29,6 +38,47 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MoveRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            direction = new Vector2(speed * Time.deltaTime, 0);
+        }
+    }
+
+    public void MoveLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            direction = new Vector2(-speed * Time.deltaTime, 0);
+        }
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            sword.SetActive(true);
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded == true)
+        {
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            isGrounded = true;
+        }
     }
 }
